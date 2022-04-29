5b336a2dc121a8d000d3a0f36041e65343a15139
talish915 <amilfn@code.edu.az>
2022-04-29 21:07:11 +0400

    first commit

---
+++ Juan Back-End Final/DAL/JuanDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Juan_Back_End_Final.DAL
+{
+    public class JuanDbContext : DbContext
+    {
+        public JuanDbContext(DbContextOptions<JuanDbContext> options): base(options)
+        {
+
+        }
+    }
+}
